c3700d4a5101911e018b225a61fa7c9d57b49a00
Linda Schasch <lasch02@sbcglobal.net>
2023-04-28 13:51:27 -0500

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace RegionalFoods.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
